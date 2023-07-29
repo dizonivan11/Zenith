9469d2159e1c9baf1261523d4d85816183f887d8
dizonivan11 <dizonivan11@gmail.com>
2023-07-30 01:42:37 +0800

    Added file and texture importer

---
+++ Components/Importer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System.IO;
+
+namespace Zenith.Components {
+    public class Importer {
+        public static Texture2D GetTexture2DFromFile(GraphicsDevice gd, string filePath) {
+            Texture2D result = null;
+            using (FileStream fs = new(filePath, FileMode.Open, FileAccess.Read)) {
+                result = Texture2D.FromStream(gd, fs);
+                fs.Close();
+            }
+            return result;
+        }
+
+        public static string GetStringFromFile(string filePath) {
+            string result = string.Empty;
+            using (StreamReader reader = new(filePath)) {
+                result = reader.ReadToEnd();
+                reader.Close();
+            }
+            return result;
+        }
+    }
+}
