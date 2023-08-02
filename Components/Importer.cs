1062b483efd05a2f61bc3fda7784a9a4755a6688
dizonivan11 <dizonivan11@gmail.com>
2023-08-02 12:14:47 +0800

    Added Texture2D extension method for saving texture slices

--- Components/Importer.cs
+++ Components/Importer.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System.IO;
 
 namespace Zenith.Components {
-    public class Importer {
+    public static class Importer {
         public static Texture2D GetTexture2DFromFile(GraphicsDevice gd, string filePath) {
             Texture2D result = null;
             using (FileStream fs = new(filePath, FileMode.Open, FileAccess.Read)) {
@@ -19,6 +20,15 @@ namespace Zenith.Components {
                 reader.Close();
             }
             return result;
+        }
+
+        public static Texture2D CreateTexture(this Texture2D texture2D, GraphicsDevice graphics, Rectangle sourceRectangle) {
+            Texture2D t = new(graphics, sourceRectangle.Width, sourceRectangle.Height);
+            int count = sourceRectangle.Width * sourceRectangle.Height;
+            Color[] data = new Color[count];
+            texture2D.GetData(0, sourceRectangle, data, 0, count);
+            t.SetData(data);
+            return t;
         }
     }
 }
