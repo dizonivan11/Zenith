f29d3316359e1c1bef14aff4a6abe6b2caec897c
dizonivan11 <dizonivan11@gmail.com>
2023-08-02 12:17:14 +0800

    Added main menu, tileset saving, tileset selection

--- Scenes/EditorScene.cs
+++ Scenes/EditorScene.cs
@@ -3,16 +3,22 @@ using Microsoft.Xna.Framework.Input;
 using Zenith.Components;
 using ImGuiNET;
 using Microsoft.Xna.Framework.Graphics;
+using NVector2 = System.Numerics.Vector2;
 
 namespace Zenith.Scenes {
     public class EditorScene : Scene {
         const int TOOLBOX_WIDTH = 90;
-        const int TILESET_WIDTH = 400;
+        const int TILESET_WIDTH = 408;
+        const int ACTION_BUFFER = 50; // Number of undo and redo action we can store
 
         TileMap tileMap;
+        Texture2D[] tiles;
+        int selectedTile;
         Texture2D selector;
         Vector2 cameraPosition;
         Vector2 cameraPositionDestination;
+
+        float mainMenuHeight;
         readonly float panSpeed;
 
         public EditorScene(MainGame mainGame) : base(mainGame) {
@@ -25,6 +31,18 @@ namespace Zenith.Scenes {
             tileMap = new TileMap(
                 Importer.GetTexture2DFromFile(mainGame.GraphicsDevice, "assets/tilesets/highlands_terrain.png"),
                 "assets/maps/highlands_0_0.txt", 32, 32);
+
+            // Create all Texture2D for all tiles which will be used by ImageButtons
+            tiles = new Texture2D[tileMap.maxTilesetX * tileMap.maxTilesetY];
+            for (int y = 0; y < tileMap.maxTilesetY; y++) {
+                for (int x = 0; x < tileMap.maxTilesetX; x++) {
+                    tiles[(y * tileMap.maxTilesetX) + x] =
+                        tileMap.tileset.CreateTexture(
+                            mainGame.GraphicsDevice,
+                            new Rectangle(x * tileMap.tileWidth, y * tileMap.tileHeight, tileMap.tileWidth, tileMap.tileHeight));
+                }
+            }
+
             selector = Importer.GetTexture2DFromFile(mainGame.GraphicsDevice, "assets/gfx/selector.png");
         }
 
@@ -38,7 +56,7 @@ namespace Zenith.Scenes {
                 dir.Normalize();
                 cameraPositionDestination = cameraPosition + dir * panSpeed * (float)gameTime.ElapsedGameTime.TotalMilliseconds;
             }
-            cameraPosition = Vector2.Lerp(cameraPosition, cameraPositionDestination, 0.008f * (float)gameTime.ElapsedGameTime.TotalMilliseconds);
+            cameraPosition = Vector2.Lerp(cameraPosition, cameraPositionDestination, 0.01f);
             
             // Snap the camera if its almost at the destination to prevent pixel jittering causing blur
             if (Vector2.Distance(cameraPosition, cameraPositionDestination) <= 1f) cameraPositionDestination = cameraPosition;
@@ -52,8 +70,8 @@ namespace Zenith.Scenes {
 
         public override void Draw() {
             tileMap.Draw(mainGame.spriteBatch, cameraPosition, mainGame.GraphicsDevice.Viewport);
-            tileMap.DrawEditor(mainGame.spriteBatch, cameraPosition, mainGame.GraphicsDevice.Viewport, selector, mainGame.guiInput, mainGame.gameInput);
-            mainGame.DrawFPSCounter(TOOLBOX_WIDTH + 10, 1);
+            tileMap.DrawEditor(mainGame.spriteBatch, cameraPosition, mainGame.GraphicsDevice.Viewport, selector, mainGame.guiInput, mainGame.gameInput, selectedTile);
+            mainGame.DrawFPSCounter(TOOLBOX_WIDTH + 10, mainMenuHeight + 10);
         }
 
         /// <summary>
@@ -61,39 +79,94 @@ namespace Zenith.Scenes {
         /// Declaring a bool is like toggling windows, setting it to true makes it visible
         /// It can also be set as false initially then control it on other widgets/functions
         /// </summary>
+        bool windowNotice = false;
+        string stringNotice = string.Empty;
         bool windowDemo = false;
         bool windowTools = true;
         bool windowTileset = true;
         public override void DrawUI(GameTime gameTime) {
+            ImGui.BeginMainMenuBar();
+            mainMenuHeight = ImGui.GetWindowSize().Y;
+            if (ImGui.BeginMenu("File")) {
+                ImGui.MenuItem("New");
+                ImGui.MenuItem("Open");
+                if (ImGui.MenuItem("Save")) {
+                    stringNotice = tileMap.SaveMapData();
+                    windowNotice = true;
+                }
+                ImGui.MenuItem("Save as");
+                ImGui.EndMenu();
+            }
+            if (ImGui.BeginMenu("Edit")) {
+                ImGui.MenuItem("Undo");
+                ImGui.MenuItem("Redo");
+                ImGui.EndMenu();
+            }
+            if (ImGui.BeginMenu("Window")) {
+                if (ImGui.MenuItem("Tools", null, windowTools)) {
+                    windowTools = !windowTools;
+                }
+                if (ImGui.MenuItem("Tileset", null, windowTileset)) {
+                    windowTileset = !windowTileset;
+                }
+                ImGui.EndMenu();
+            }
+            ImGui.EndMainMenuBar();
+
+            if (windowNotice) {
+                ImGui.SetNextWindowPos(new NVector2(
+                    mainGame.GraphicsDevice.Viewport.Width / 2,
+                    mainGame.GraphicsDevice.Viewport.Height / 2),
+                    ImGuiCond.Always, new NVector2(0.5f, 0.5f));
+                ImGui.Begin("Notice",
+                        ImGuiWindowFlags.NoResize |
+                        ImGuiWindowFlags.NoSavedSettings |
+                        ImGuiWindowFlags.Modal |
+                        ImGuiWindowFlags.NoCollapse);
+                ImGui.Text(stringNotice);
+                if (ImGui.Button("Ok")) windowNotice = false;
+                ImGui.End();
+            }
+
             if (windowDemo)
                 ImGui.ShowDemoWindow(ref windowDemo);
 
             if (windowTools) {
-                ImGui.SetNextWindowPos(new System.Numerics.Vector2(0, 0), ImGuiCond.None);
-                ImGui.SetNextWindowSize(new System.Numerics.Vector2(TOOLBOX_WIDTH, mainGame.GraphicsDevice.Viewport.Height));
+                ImGui.SetNextWindowPos(new NVector2(0, mainMenuHeight), ImGuiCond.None);
+                ImGui.SetNextWindowSize(new NVector2(TOOLBOX_WIDTH, mainGame.GraphicsDevice.Viewport.Height - mainMenuHeight));
                 ImGui.Begin("Tools",
                     ref windowTools,
                     ImGuiWindowFlags.NoSavedSettings |
                     ImGuiWindowFlags.NoMove |
                     ImGuiWindowFlags.NoResize);
-                ImGui.Button("Select", new System.Numerics.Vector2(-1, 20));
-                ImGui.Button("Move", new System.Numerics.Vector2(-1, 20));
-                ImGui.Button("Brush", new System.Numerics.Vector2(-1, 20));
-                ImGui.Button("Erase", new System.Numerics.Vector2(-1, 20));
-                ImGui.Button("Fill", new System.Numerics.Vector2(-1, 20));
+                ImGui.Button("Select", new NVector2(-1, 20));
+                ImGui.Button("Move", new NVector2(-1, 20));
+                ImGui.Button("Brush", new NVector2(-1, 20));
+                ImGui.Button("Erase", new NVector2(-1, 20));
+                ImGui.Button("Fill", new NVector2(-1, 20));
                 ImGui.End();
             }
             if (windowTileset) {
-                ImGui.SetNextWindowPos(new System.Numerics.Vector2(mainGame.GraphicsDevice.Viewport.Width - TILESET_WIDTH, 0), ImGuiCond.None);
-                ImGui.SetNextWindowSize(new System.Numerics.Vector2(TILESET_WIDTH, mainGame.GraphicsDevice.Viewport.Height));
+                ImGui.SetNextWindowPos(new NVector2(mainGame.GraphicsDevice.Viewport.Width - TILESET_WIDTH, mainMenuHeight), ImGuiCond.None);
+                ImGui.SetNextWindowSize(new NVector2(TILESET_WIDTH, mainGame.GraphicsDevice.Viewport.Height - mainMenuHeight));
                 ImGui.Begin("Tileset",
                     ref windowTileset,
                     ImGuiWindowFlags.NoSavedSettings |
                     ImGuiWindowFlags.NoMove |
                     ImGuiWindowFlags.NoResize |
-                    ImGuiWindowFlags.AlwaysHorizontalScrollbar |
+                    ImGuiWindowFlags.HorizontalScrollbar |
                     ImGuiWindowFlags.AlwaysVerticalScrollbar);
-                ImGui.Image(mainGame.ui.BindTexture(tileMap.tileset), new System.Numerics.Vector2(tileMap.tileset.Width, tileMap.tileset.Height));
+                for (int y = 0; y < tileMap.maxTilesetY; y++) {
+                    for (int x = 0; x < tileMap.maxTilesetX; x++) {
+                        int t = (y * tileMap.maxTilesetX) + x;
+                        if (ImGui.ImageButton(string.Format("{0}-{1}", x, y),
+                            mainGame.ui.BindTexture(tiles[t]),
+                            new NVector2(tileMap.tileWidth, tileMap.tileHeight))) {
+                            selectedTile = t;
+                        }
+                        if ((t + 1) % tileMap.maxTilesetX != 0) ImGui.SameLine();
+                    }
+                }
                 ImGui.End();
             }
         }
